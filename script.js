9dca2ef46ccf2ef8c67c20b2406f70b80dfcd8cf
MarquesF1 <92760949+MarquesF1@users.noreply.github.com>
2021-10-22 11:26:57 -1000

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function username() {
   //create local storage key with value of "a"
   localStorage.setItem('uName', a);
   //automatically take user to page two.html
-  window.alert(item)
+  window.alert(a)
   location.href = "two.html"
+}
+function getUser() {
+  var b = localStorage.getItem('uName');
+  document.getElementById('placeholder').innerHTML = "Hello " + b;
 }
