130c407aff48539f7706cfb8b271ef97796fbb26
MarquesF1 <92760949+MarquesF1@users.noreply.github.com>
2021-10-22 11:19:50 -1000

    used local storage

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+var a;
 function username() {
-  var item = document.getElementById('name').value;
-  document.getElementById('name').value = "";
+  //get value from input field and save as "a"
+  a = document.getElementById('name').value;
+  //create local storage key with value of "a"
+  localStorage.setItem('uName', a);
+  //automatically take user to page two.html
   window.alert(item)
+  location.href = "two.html"
 }
